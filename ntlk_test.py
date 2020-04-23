e36ebd06e5eed6aa86632b48d89fd23dcf1c8ae7
Eric Sturman <eric.sturman@gmail.com>
2020-04-22 21:00:50 -0500

    Update ntlk_test.py

--- ntlk_test.py
+++ ntlk_test.py
@@ -1,4 +1,5 @@
 import nltk
+from nltk.data import load
 # nltk.download('punkt')
 sentence = """At eight o'clock on Thursday morning ... Arthur didn't feel very good."""
 tokens = nltk.word_tokenize(sentence)
@@ -8,4 +9,6 @@ tagged = nltk.pos_tag(tokens)
 print(tagged[0:6])
 # nltk.download('tagsets')
 # nltk.help.upenn_tagset()
-# tagdict = load("help/tagsets/upenn_tagset.pickle")
+tagdict = load("help/tagsets/upenn_tagset.pickle")
+print(tagdict['NN'][0])
+print(tagdict['NN'][1])
